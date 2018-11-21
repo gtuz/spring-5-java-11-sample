a1acdcc8ec45f08f9233e9eaf2d710741c9ce8d5
gtuz <45243898+gtuz@users.noreply.github.com>
2018-11-21 21:57:12 +0100

    commit sample project from initializr

---
+++ src/main/java/com/github/gtuz/spring5java11sample/Spring5Java11SampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.gtuz.spring5java11sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Java11SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5Java11SampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/gtuz/spring5java11sample/Spring5Java11SampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.gtuz.spring5java11sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5Java11SampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
